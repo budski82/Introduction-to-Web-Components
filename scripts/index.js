2c69fee8efa3505db5dffe60b3eaba6d402b0323
William Rutkowski <budski82@users.noreply.github.com>
2023-02-08 13:26:41 -0500

    4th

---
+++ scripts/index.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    console.log(`Main.js and DOM are loaded`);
+})
--- scripts/webComp.js
+++ scripts/webComp.js
@@ -1,9 +1,28 @@
 // Web Component file, it only need to run
 // doesn't require being imported to main.js
 
+const template = document.createElement('template')
+template.innerHTML=`
+  <div>
+	  <h1>BOO Big Bang BOO</h1>
+		<slot name="title">Some default fallback text</slot>
+		<slot name="items"
+	</div>
+`
+
+
+
+
 class WebComp extends HTMLElement{
 	constructor(){
 		super()
+    
+		const shadowRoot = this.attachShadow({'mode': 'closed'})
+		// let div = document.createElement('div')
+		// div.textContent = `Big Bang Boom`
+		// shadowRoot.append(div)
+    let clone = template.content.cloneNode(true)
+		shadowRoot.append(clone)
 
 	}
 }
